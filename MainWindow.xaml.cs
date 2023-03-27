c671f1099b8d57a252d73709901cd5e1aec3ff5c
WASDyt <122372935+WASDyt@users.noreply.github.com>
2023-03-27 19:28:12 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppPractwork1.Pages;
+
+namespace WpfAppPractwork1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            WindowPassword passwordWindow = new WindowPassword();
+            StreamReader sr = new StreamReader(@"Passwort.psw");
+            if (passwordWindow.ShowDialog() == true)
+            {
+                if (passwordWindow.Password == sr.ReadLine())
+                {
+                    MessageBox.Show("Авторизация пройдена");
+                    ClassFrame.frmObj = FrmMain;
+                    ClassFrame.frmObj.Navigate(new PageMain());
+                }
+
+                else
+                    MessageBox.Show("Неверный пароль");
+            }
+            else
+            {
+                MessageBox.Show("Авторизация не пройдена");
+            }
+        }
+    }
+}
---
+++ Pages/Dialog.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppPractwork1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Dialog.xaml
+    /// </summary>
+    public partial class Dialog : Page
+    {
+        public Dialog()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            if (ofd.ShowDialog() == false)
+                return;
+            string filename = ofd.FileName;
+            string fileText = System.IO.File.ReadAllText(filename);
+
+            MessageBox.Show($"Файл {filename} открыт. Пароль: {fileText}");
+            
+        }
+    }
+}
